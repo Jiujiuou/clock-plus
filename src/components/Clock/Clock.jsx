4f5f0ded57b0bb92520f22835014da01da7aa9e2
cairuihao <cairuihao@shouqianba.com>
2024-12-25 18:02:20 +0800

    feat: 添加秒针个位十位

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -2,21 +2,25 @@ import { useEffect, useState } from "react";
 import styles from "./index.module.less";
 import clsx from "clsx";
 
-const list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const secondUnitsList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const secondTensList = [0, 1, 2, 3, 4, 5];
 
 function Header() {
-  const [second, setSecond] = useState(0);
-  const [rotation, setRotation] = useState(0);
+  const [secondUnits, setSecondUnits] = useState(0);
+  const [secondTens, setSecondTens] = useState(0);
+  const [rotationSecondsUnit, setRotationSecondsUnit] = useState(0);
+  const [rotationSecondsTen, setRotationSecondsTen] = useState(0);
 
   const getSecondsUnit = () => {
     const now = new Date();
     const seconds = now.getSeconds();
     const secondsUnit = seconds % 10;
+    const secondsTens = Math.floor(seconds / 10);
 
-    // 计算需要旋转的角度，使得当前数字旋转到0的位置
-    const targetRotation = secondsUnit * 20;
-    setRotation(targetRotation);
-    setSecond(secondsUnit);
+    setRotationSecondsUnit(secondsUnit * 20);
+    setRotationSecondsTen(secondsTens * 20);
+    setSecondUnits(secondsUnit);
+    setSecondTens(secondsTens);
   };
 
   useEffect(() => {
@@ -29,23 +33,26 @@ function Header() {
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.second}>{second}</div>
+      <div className={styles.timeShow}>
+        {secondTens}-{secondUnits}
+      </div>
+
       <div className={styles.circleWrapper}>
         <div
           className={styles.numberWrapper}
           style={{
-            transform: `rotate(${rotation}deg)`,
+            transform: `rotate(${rotationSecondsUnit}deg)`,
             transition: "transform 0.5s ease",
           }}
         >
-          {list.map((item, index) => {
+          {secondUnitsList.map((item, index) => {
             const angle = index * 20;
             return (
               <p
                 key={item}
                 className={clsx(
                   styles.circleNumber,
-                  index === second && styles.active
+                  index === secondUnits && styles.active
                 )}
                 style={{
                   transform: `
@@ -55,10 +62,49 @@ function Header() {
                   `,
                 }}
               >
-                <span 
-                  style={{ 
-                    display: 'block',
-                    transform: `rotate(${-angle}deg)`
+                <span
+                  style={{
+                    display: "block",
+                    transform: `rotate(${-angle}deg)`,
+                  }}
+                >
+                  {item}
+                </span>
+              </p>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className={styles.circleWrapper}>
+        <div
+          className={styles.numberWrapper}
+          style={{
+            transform: `rotate(${rotationSecondsTen}deg)`,
+            transition: "transform 0.5s ease",
+          }}
+        >
+          {secondTensList.map((item, index) => {
+            const angle = index * 20;
+            return (
+              <p
+                key={item}
+                className={clsx(
+                  styles.circleNumber,
+                  index === secondTens && styles.active
+                )}
+                style={{
+                  transform: `
+                    rotate(${-angle}deg)
+                    translate(100px)
+                    rotate(${angle}deg)
+                  `,
+                }}
+              >
+                <span
+                  style={{
+                    display: "block",
+                    transform: `rotate(${-angle}deg)`,
                   }}
                 >
                   {item}
