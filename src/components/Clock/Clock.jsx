02589d2fbca4e416e2ddb80c8fdca951ee1598d0
cairuihao <cairuihao@shouqianba.com>
2024-12-25 17:29:32 +0800

    feat: 新增滚动动画

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -1,9 +1,63 @@
+import { useEffect, useState } from "react";
 import styles from "./index.module.less";
+import clsx from "clsx";
+
+const list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function Header() {
+  const [second, setSecond] = useState(0);
+  const [rotation, setRotation] = useState(0);
+
+  const getSecondsUnit = () => {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsUnit = seconds % 10;
+
+    // 计算需要旋转的角度，使得当前数字旋转到0的位置
+    const targetRotation = secondsUnit * 20;
+    setRotation(targetRotation);
+    setSecond(secondsUnit);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(getSecondsUnit, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.cycle}></div>
+      <div className={styles.second}>{second}</div>
+      <div className={styles.circleWrapper}>
+        <div
+          className={styles.numberWrapper}
+          style={{
+            transform: `rotate(${rotation}deg)`,
+            transition: "transform 0.5s ease",
+          }}
+        >
+          {list.map((item, index) => {
+            return (
+              <p
+                key={item}
+                className={clsx(
+                  styles.circleNumber,
+                  index === second && styles.active
+                )}
+                style={{
+                  transform: `rotate(-${
+                    index * 20
+                  }deg) translate(100px) rotate(${index * 20}deg)`,
+                }}
+              >
+                {item}
+              </p>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
